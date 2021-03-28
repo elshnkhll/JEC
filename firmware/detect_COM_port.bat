05273a8060a1a81ac63a524e26005313f27856eb
ELSHNKHLL <elshnkhll@yahoo.com>
2021-03-28 16:04:54 -0400

    Update detect_COM_port.bat
    Detects the COM port which is your device is connected to.

--- firmware/detect_COM_port.bat
+++ firmware/detect_COM_port.bat
@@ -3,13 +3,11 @@ echo ***********************
 echo * (c) RoboCallz, 2017 *
 echo ***********************
 setlocal
-
 for /f "delims=" %%I in (
 'wmic path Win32_SerialPort Where "Description LIKE 'Silicon Labs CP210%%'" get DeviceID^,Description^ /format:list ^| find "="'
 ) do (
     set "%%I"
 )
-
 IF "%DeviceID%" == "" ( 
 	ECHO No Device found
 	PAUSE
